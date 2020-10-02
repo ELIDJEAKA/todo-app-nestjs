005bb48e56bf1c0b9d951869c363590e8da7e229
ELIDJE AKA EMMANUEL <elidjeakaemmanuel@gmail.com>
2020-10-02 02:58:39 +0200

    modification d'un todo existant

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,7 @@
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodosService } from './todos.service';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Controller,Get, Post, Body, Param } from '@nestjs/common';
+import { Controller,Get, Post, Body, Param, Patch } from '@nestjs/common';
 import { Todo } from './interfaces/todos.interface';
 
 
@@ -25,6 +25,11 @@ export class TodosController {
     createTodo(@Body() newTodo:CreateTodoDto){
         console.log('newTodo : ', newTodo);
         this.todosService.create(newTodo);
+    }
+
+    @Patch(':id')
+    updateTodo(@Param('id') id:string, @Body() todo:CreateTodoDto){
+        return this.todosService.update(id,todo)
     }
 
 
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,5 +1,5 @@
 import { CreateTodoDto } from './dto/create-todo.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './interfaces/todos.interface';
 
 @Injectable()
@@ -35,5 +35,28 @@ export class TodosService {
 
     findOne(id:string){
        return this.todos.find(todo=>todo.id===Number(id))
+    }
+
+    update(id:string,todo:Todo){
+        const todoToUpdate = this.todos.find(t=>t.id=== +id)
+        if (!todoToUpdate){
+            return new NotFoundException('Ce todo est inexistant !')
+        }
+
+        if(todo.hasOwnProperty('done')){
+            todoToUpdate.done = todo.done;
+        }
+
+        if (todo.title){
+            todoToUpdate.title = todo.title
+        }
+        if (todo.description){
+            todoToUpdate.description = todo.description
+        }
+        
+
+        const updatedTodos = this.todos.map(t=>t.id !== +id ? t : todoToUpdate)
+        this.todos = [...updatedTodos]
+        return {updatedTodo:1,todo:todoToUpdate}
     }
 }
